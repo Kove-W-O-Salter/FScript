01984016ea712bd3110c0d79926400f8ef627fde
Kove W. Ochre-Salter <kove.w.o.salter@gmail.com>
2018-06-18 18:43:25 +1000

    Finnish Parser (FScript/Parser.hs)

--- FScript/Parser.hs
+++ FScript/Parser.hs
@@ -21,5 +21,162 @@ module FScript.Parser where
 import FScript.Expr
 
 --
--- 
+-- @Reason        : To parse synatx we need a parser
+--                  and Parsec is my favourite.
+-- @Creation Date : 18'th June 2018.
 --
+
+import Text.Parsec
+
+--
+-- @Reason        : The Identity Monad is just for abstracting of ParsecT.
+-- @Creation Date : 18'th June 2018.
+--
+
+import Control.Monad.Identity
+
+--
+-- @Decription    : Abstracting over Parsec's default Parsing
+--                  type since it's not easy to read.
+-- @Creation Date : 18'th June 2018.
+--
+
+type Parser a = ParsecT String () Identity a
+
+--
+-- @Description   : Parse an expression.
+-- @Creation Date : 18'th June 2018.
+--
+expression :: Parser Expr
+expression  = (try constant) <|> (try application) <|> (try declaration) <|> parenthesis
+
+--
+-- @Description   : Match a constant.
+-- @Creation Date : 18'th June 2018.
+--
+
+constant :: Parser Expr
+constant  = do i <- integer
+               return $ Con i
+
+--
+-- @Description   : Match an integer value.
+-- @Creation Date : 18'th June 2018.
+--
+
+integer :: Parser Int
+integer  = do is <- digits
+              notFollowedBy letter
+              return $ read is
+
+--
+-- @Description   : Match one or more digits.
+-- @Creation Date : 18'th June 2018.
+--
+
+digits :: Parser String
+digits  = many1 digit
+
+--
+-- @Description   : Match an application.
+-- @Creation Date : 18'th June 2018.
+--
+
+application :: Parser Expr
+application  = (try idApplication) <|> opApplication
+
+--
+-- @Description   : Match the application of an identifier
+--                  to zero or more arguments.
+-- @Creation Date : 18'th June 2018.
+--
+
+idApplication :: Parser Expr
+idApplication  = do i <- identifier
+                    space
+                    as <- arguments
+                    notFollowedBy (letter <|> digit)
+                    return $ Cal i as
+
+--
+-- @Description   : Match an identifier.
+-- @Creation Date : 18'th June 2018.
+--
+
+identifier :: Parser String
+identifier  = do l <- letter
+                 dl <- many (letter <|> digit)
+                 return $ l : dl
+
+--
+-- @Description   : Match zero or more expressions separated by semicolons.
+-- @Creation Date : 18'th June 2018.
+--
+
+arguments :: Parser [Expr]
+arguments  = do as <- sepBy expression (char ' ')
+                notFollowedBy (letter <|> digit)
+                return as
+
+--
+-- @Description   : Match the application of an operator to
+--                  one or more arguments.
+-- @Creation Date : 18'th June 2018.
+--
+
+opApplication :: Parser Expr
+opApplication  = do char '['
+                    o <- operator
+                    char ']'
+                    space
+                    as <- arguments
+                    notFollowedBy (letter <|> digit)
+                    return $ App o as
+
+--
+-- @Description   : Match an operator.
+-- @Creation Date : 18'th June 2018.
+--
+
+operator :: Parser Oper
+operator  = do o <- oneOf "+-*/"
+               return $ readOp o
+
+--
+-- @Description   : Read an operator.
+-- @Creation Date : 18'th June 2018.
+--
+
+readOp     :: Char -> Oper
+readOp '+'  = Add
+readOp '-'  = Sub
+readOp '*'  = Mul
+readOp '/'  = Div
+
+--
+-- @Description   : Match the declaration of an identifier.
+-- @Creation Date : 18'th June 2018.
+--
+
+declaration :: Parser Expr
+declaration  = do char '['
+                  i <- identifier
+                  space
+                  as <- sepBy identifier space
+                  char ']'
+                  space
+                  string "=>"
+                  space
+                  b <- expression
+                  spaces
+                  e <- expression
+                  notFollowedBy (letter <|> digit)
+                  return $ Dec i as b e
+
+--
+-- @Description   : Match an expression between parenthesis.
+-- @Creation Date : 18'th June 2018.
+--
+
+parenthesis :: Parser Expr
+parenthesis  = between (string "(") (string ")") expression
